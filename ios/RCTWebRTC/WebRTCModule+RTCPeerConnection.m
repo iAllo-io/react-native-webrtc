003e63f33c5638c4384b39a25b962759270e49ae
ShivaniAddval <53002940+shivi126@users.noreply.github.com>
2023-12-06 14:24:03 +0530

    DTMF Fix for undefined number-15

--- ios/RCTWebRTC/WebRTCModule+RTCPeerConnection.m
+++ ios/RCTWebRTC/WebRTCModule+RTCPeerConnection.m
@@ -355,6 +355,12 @@ RCT_EXPORT_METHOD(peerConnectionSendDTMF:(nonnull NSString *)tone duration:(NSTi
   }
   if(audioSender){
     if([audioSender.dtmfSender canInsertDtmf]){
+
+
+    if([tone hasPrefix:@"undefined"]) {
+        tone = [tone substringFromIndex:@"undefined".length];
+    }
+
       BOOL istoneplayed = [audioSender.dtmfSender insertDtmf:tone duration:duration interToneGap:interToneGap];
       NSLog(@"DTMF Tone played :: [%s]", istoneplayed ? "true" : "false");
       return;
